c200a3f988eb5890091312b306eacf5e6cdfea67
Nick Sergeev <nick@manticoresearch.com>
2022-09-15 22:37:07 +0700

    Updated tests

--- src/ManticoreSearch.Test/Api/IndexApiTests.cs
+++ src/ManticoreSearch.Test/Api/IndexApiTests.cs
@@ -34,7 +34,7 @@ namespace ManticoreSearch.Test.Api
     public class IndexApiTests : IDisposable
     {
         private IndexApi instance;
-        private Dictionary<string, Func<Object>> implementedTests;
+        private Dictionary<string, Dictionary<string,Func<Object>>> implementedTests;
 
         private object InitTests()
         {
