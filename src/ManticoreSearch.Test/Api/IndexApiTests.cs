f9eee081b12e11de2aa6a7aa6692f1792cb52d8e
Nick Sergeev <nick@manticoresearch.com>
2022-09-15 17:49:05 +0700

    Updated tests

--- src/ManticoreSearch.Test/Api/IndexApiTests.cs
+++ src/ManticoreSearch.Test/Api/IndexApiTests.cs
@@ -51,9 +51,9 @@ namespace ManticoreSearch.Test.Api
                 
         private static Dictionary<string, Action> implementedTests = new Dictionary<string, Action>()
         {
-            { 'IndexApiTests', () => { this.InitTests(); } },
-            { 'SearchApiTests', () => { this.InitTests(); } },
-            { 'UtilsTests', () => { this.InitTests(); } },
+            { "IndexApiTests", () => { this.InitTests(); } },
+            { "SearchApiTests", () => { this.InitTests(); } },
+            { "UtilsTests", () => { this.InitTests(); } },
         };
         
         private void Checktest(testName)
