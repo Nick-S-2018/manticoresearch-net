123c2aaaa791ae2e1d856e4818f1c21fa1c64578
Nick Sergeev <nick@manticoresearch.com>
2022-09-15 18:11:58 +0700

    Updated tests

--- src/ManticoreSearch.Test/Api/IndexApiTests.cs
+++ src/ManticoreSearch.Test/Api/IndexApiTests.cs
@@ -14,7 +14,9 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
+using System.Net.Http
 using Xunit;
+
 
 using ManticoreSearch.Client;
 using ManticoreSearch.Api;
--- src/ManticoreSearch.Test/Api/SearchApiTests.cs
+++ src/ManticoreSearch.Test/Api/SearchApiTests.cs
@@ -14,6 +14,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Linq;
 using System.Reflection;
+using System.Net.Http
 using Xunit;
 
 using ManticoreSearch.Client;
